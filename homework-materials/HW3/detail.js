d6f2ecb44867c07db652035ec8a620d9f3119601
Yizhe Liu <yizhel@andrew.cmu.edu>
2023-02-10 23:49:03 -0500

    Started HW 3

---
+++ homework-materials/HW3/detail.js
@@ -0,0 +1,37 @@
+let glazeOptions = [
+  {
+    option: "Keep Original",
+    price: 0,
+  },
+  {
+    option: "Sugar Milk",
+    price: 0,
+  },
+  {
+    option: "Vanilla Milk",
+    price: 0.5,
+  },
+  {
+    option: "Double Chocolate",
+    price: 1.5,
+  },
+];
+
+let packOptions = [
+  {
+    option: "1",
+    price: 1,
+  },
+  {
+    option: "3",
+    price: 3,
+  },
+  {
+    option: "6",
+    price: 5,
+  },
+  {
+    option: "12",
+    price: 10,
+  },
+];
